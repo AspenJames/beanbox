bf064c39ce6639125a7dd6bf811127fdbf3b13fd
Aspen <aspenjames@tqca.org>
2018-03-27 14:46:35 -0700

    Wrote CLI functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,10 +1,29 @@
 class Beanbox::CLI
+
+  def initialize
+    Beanbox::Coffee.reset!
+  end
   def run
     self.welcome_message
-    scraper = Beanbox::Scraper.new
+    Beanbox::Scraper.new("https://beanbox.co/coffee")
+    self.list_coffees
+    # self.list_menu #Update later to expand functionality
   end
 
   def welcome_message
     puts "Hello! Today's top selling coffees are: "
   end
+
+  def list_coffees
+    Beanbox::Coffee.all.each_with_index do |c, i|
+      puts "#{i + 1}. #{c.name} roasted by #{c.roaster} at #{c.price}"
+      puts
+    end
+  end
+
+  # def list_menu
+  #  #Update later to expand functionality
+  # end
+
+
 end
