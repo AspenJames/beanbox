ff2d92927a8a3554adcb95621a4a17c4498da446
Aspen <aspenjames@tqca.org>
2018-03-27 10:43:14 -0700

    Updated CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,10 @@
 class Beanbox::CLI
-  
+  def run
+    self.welcome_message
+    scraper = Beanbox::Scraper.new
+  end
+
+  def welcome_message
+    puts "Hello! Today's top selling coffees are: "
+  end
 end
