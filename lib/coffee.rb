e7cd0d78c2e64c9551d6460f292b737f6ea66a74
Aspen <aspenjames@tqca.org>
2018-03-27 12:09:06 -0700

    Wrote initial functionality

--- lib/coffee.rb
+++ lib/coffee.rb
@@ -1,4 +1,16 @@
 class Beanbox::Coffee
   attr_accessor :name, :roaster, :price
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset!
+    @@all = []
+  end
 end
