cc7430f50fef816e86362fd0815ea379ed8e49f6
Aspen <aspenjames@tqca.org>
2018-03-29 18:16:26 -0700

    Fixed file requirements after relocating

--- lib/beanbox.rb
+++ lib/beanbox.rb
@@ -1,7 +1,8 @@
 require "beanbox/version"
-require "cli"
-require "coffee"
-require "scraper"
+require_relative "./beanbox/cli"
+require_relative "./beanbox/coffee"
+require_relative "./beanbox/scraper"
+
 module Beanbox
   # Your code goes here...
 end
